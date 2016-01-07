ee1b0070135d346c0469bafe2c99e5971010b1d9
Connor Riley <connor.riley@uconn.edu>
2016-01-07 16:41:26 -0500

    added supplementerly files for last commit

---
+++ app/src/main/java/com/speakr/connorriley/speakr/JamListActivity.java
@@ -0,0 +1,180 @@
+package com.speakr.connorriley.speakr;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+
+public class JamListActivity extends HamburgerActivity implements OnClickListener{
+
+    DrawerLayout drawerLayout;
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    Toolbar toolbar;
+    TabLayout tabLayout;
+    FloatingActionButton fab;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jamlist);
+        setupNavigationView();
+        setupToolbar();
+        //setupTablayout();
+        //setupCollapsingToolbarLayout();
+        setupFab();
+
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(JamListActivity.this, CreateJamActivity.class);
+                startActivity(intent);
+            }
+        });*/
+
+        /*listView = (ListView)findViewById(R.id.jams_list);
+        String[] values = new String[] {
+                "Jam 1",
+                "Jam 2",
+                "Bangers",
+                "Club mix"
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_expandable_list_item_1,
+                android.R.id.text1,
+                values
+        );
+        listView.setAdapter(adapter);*/
+
+        //addDrawerItems();
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case android.R.id.home:
+                if(drawerLayout != null)
+                    drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupNavigationView(){
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navView = (NavigationView) findViewById(R.id.navList);
+        navView.setNavigationItemSelectedListener(this);
+    }
+
+    /*private void setupCollapsingToolbarLayout(){
+
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        if(collapsingToolbarLayout != null){
+            collapsingToolbarLayout.setTitle(toolbar.getTitle());
+            //collapsingToolbarLayout.setCollapsedTitleTextColor(0xED1C24);
+            //collapsingToolbarLayout.setExpandedTitleColor(0xED1C24);
+        }
+    }*/
+
+    /*private void setupTablayout(){
+
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+
+        if(tabLayout == null)
+            return;
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 4"));
+    }*/
+
+    private void setupFab(){
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        if(fab != null)
+            fab.setOnClickListener(this);
+    }
+    private void setupToolbar(){
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if(toolbar != null)
+            setSupportActionBar(toolbar);
+
+        // Show menu icon
+        final ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public void onClick(View view) {
+
+        if(view.getId() == R.id.fab){
+
+            Intent intent = new Intent(JamListActivity.this, CreateJamActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    public void openPlayerActivity(){
+        //-- Mike 10/28/15
+        Intent intent = new Intent(this, PlayerActivity.class);
+        startActivity(intent);
+    }
+
+    public void openWifiActivity(){
+        //-- Mike 1/6/16
+        Intent intent = new Intent(this, WiFiDirectActivity.class);
+        startActivity(intent);
+    }
+
+   @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.nav_jams:
+                break;
+            case R.id.nav_music_player:
+                openPlayerActivity();
+                break;
+            case R.id.nav_wifi:
+                openWifiActivity();
+                break;
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
