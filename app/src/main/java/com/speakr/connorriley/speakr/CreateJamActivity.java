da9948dc20dc1b293f8687976c31d51626de4235
Drew <robert.paganetti@uconn.edu>
2016-01-07 14:29:12 -0500

    createJamActivity takes input from user and goes to music player

--- app/src/main/java/com/speakr/connorriley/speakr/CreateJamActivity.java
+++ app/src/main/java/com/speakr/connorriley/speakr/CreateJamActivity.java
@@ -1,13 +1,21 @@
 package com.speakr.connorriley.speakr;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by connorriley on 1/7/16.
@@ -27,6 +35,64 @@ public class CreateJamActivity extends HamburgerActivity {
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //Drew - 1/7/16
+        final EditText nicknameEditText = (EditText) findViewById(R.id.nickname);
+        final EditText jamnameEditText = (EditText) findViewById(R.id.jam_name);
+
+        Button doneButton = (Button) findViewById(R.id.done);
+        doneButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                nicknameEditText.setVisibility(View.INVISIBLE);
+                jamnameEditText.setVisibility(View.INVISIBLE);
+                if (nicknameEditText.getText().toString().length() == 0) {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Please enter a nickname!";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+
+                else if(jamnameEditText.getText().toString().length() == 0) {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Please enter a jam name!";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+
+                else {
+
+                    /*Context context = getApplicationContext();
+                    CharSequence text = "Both fields non-null";
+                    int duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();*/
+
+                    openPlayerActivity();
+                }
+
+                nicknameEditText.setVisibility(View.VISIBLE);
+                jamnameEditText.setVisibility(View.VISIBLE);
+            }
+        });
+
+        /*
+        nicknameEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+
+        @Override
+        public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+            boolean handled = false;
+            if (i == EditorInfo.IME_ACTION_NEXT) {
+                //show toast for input
+                String inputText = textView.getText().toString();
+                Toast.makeText(CreateJamActivity.this, "Your nickname is: " + inputText, Toast.LENGTH_SHORT).show();
+                handled = true;
+            }
+                return handled;
+            }
+        });*/
     }
     public void openPlayerActivity(){
         //-- Mike 10/28/15
