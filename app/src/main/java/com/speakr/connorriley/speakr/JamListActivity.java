c5b2a627c1b3c29ec03de36147bc38dd2814cdff
Connor Riley <connor.riley@uconn.edu>
2016-01-07 21:35:14 -0500

    fixed hamburger menu icon on home screen

--- app/src/main/java/com/speakr/connorriley/speakr/JamListActivity.java
+++ app/src/main/java/com/speakr/connorriley/speakr/JamListActivity.java
@@ -10,6 +10,7 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -124,9 +125,11 @@ public class JamListActivity extends HamburgerActivity implements OnClickListene
             setSupportActionBar(toolbar);
 
         // Show menu icon
-        final ActionBar ab = getSupportActionBar();
-        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
-        ab.setDisplayHomeAsUpEnabled(true);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
     }
 
 
