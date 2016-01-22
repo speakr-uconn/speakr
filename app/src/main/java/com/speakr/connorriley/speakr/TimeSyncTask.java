92b84fd83f6b039213a1a5eee8a8e3725294c794
Viren Shinde <vpsxslash@gmail.com>
2016-01-22 10:56:30 -0500

    converted server and system time to readable date format

--- app/src/main/java/com/speakr/connorriley/speakr/TimeSyncTask.java
+++ app/src/main/java/com/speakr/connorriley/speakr/TimeSyncTask.java
@@ -2,7 +2,13 @@ package com.speakr.connorriley.speakr;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.text.format.Time;
 import android.util.Log;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by Viren on 1/22/2016.
@@ -17,6 +23,12 @@ public class TimeSyncTask extends AsyncTask<TimeSync, Void, Long> {
         long time = timeSync.getNTPTime();
         if (time != -1) {
             Log.d(LOG_TAG, "Time received: " + time);
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTimeInMillis(time);
+            DateFormat df = new SimpleDateFormat("dd:MM:yy:HH:mm:ss.SSS");
+
+            Log.d(LOG_TAG, "Server time: " + df.format(calendar.getTime()));
+            Log.d(LOG_TAG, "System time: " + df.format(System.currentTimeMillis()));
         } else {
             Log.e(LOG_TAG, "ERROR: time received was " + time);
         }
